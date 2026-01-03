397a789c7b2e041a9845938431b2b0bd14d8aa6f
Saksham <sakshamsaxena16@gmail.com>
2026-01-03 15:13:32 +0530

    Added VPC Module

---
+++ envs/prod/main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  env                     = var.env
+  vpc_cidr                = var.vpc_cidr
+  azs                     = var.azs
+  public_subnet_cidrs     = var.public_subnet_cidrs
+  private_subnet_cidrs    = var.private_subnet_cidrs
+  private_db_subnet_cidrs = var.private_db_subnet_cidrs
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,30 @@
+variable "env" {
+  description = "The environment for the VPC (e.g., dev, prod)"
+  type        = string
+  
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "A list of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "A list of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "A list of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "private_db_subnet_cidrs" {
+  description = "A list of CIDR blocks for private database subnets"
+  type        = list(string)
+}
