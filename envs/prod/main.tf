7b6e0af9b1251de332cf5b39d334c320c124309e
Saksham <sakshamsaxena16@gmail.com>
2026-01-11 22:53:34 +0530

    fixed ami

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 module "security" {
   source = "../../modules/security"
 
-  env    = var.env
+  env   = var.env
   vpc_id = module.vpc.vpc_id
 }
 
@@ -23,21 +23,19 @@ module "alb" {
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnet_ids
   alb_sg_id         = module.security.alb_sg_id
-  domain_name       = var.domain_name
-  hosted_zone_name  = var.hosted_zone_name
 }
 
 module "compute" {
   source = "../../modules/compute"
-
-  env                  = var.env
-  ami_id               = var.ami_id
-  instance_type        = var.instance_type
-  private_subnet_ids   = module.vpc.private_subnet_ids
-  app_sg_id            = module.security.app_sg_id
-  target_group_arn     = module.alb.target_group_arn
-  asg_max_size         = var.asg_max_size
-  asg_min_size         = var.asg_min_size
+# ami
+  env                = var.env
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  private_subnet_ids = module.vpc.private_subnet_ids
+  app_sg_id          = module.security.app_sg_id
+  target_group_arn   = module.alb.target_group_arn
+  asg_max_size       = var.asg_max_size
+  asg_min_size       = var.asg_min_size
   asg_desired_capacity = var.asg_desired_capacity
 }
 
@@ -51,5 +49,15 @@ module "rds" {
   db_instance_class = var.db_instance_class
 
   db_subnet_ids = module.vpc.private_db_subnet_ids
-  db_sg_ids     = module.security.db_sg_ids
+  db_sg_ids      = module.security.db_sg_ids
+}
+
+module "monitoring" {
+  source = "../../modules/monitoring"
+
+  env                     = var.env
+  email                   = var.email
+  asg_name                = module.compute.asg_name
+  alb_arn_suffix          = module.alb.alb_arn_suffix
+  target_group_arn_suffix = module.alb.target_group_arn_suffix
 }
